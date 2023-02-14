6c42b804ee5c61b07091df730c7c90835169ff39
Ivan <ivan@spasov.xyz>
2023-02-14 20:55:30 +0200

    .

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -8,7 +8,7 @@ import Logo from "../Logo/Logo";
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <Logo />
+      
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
