c60dbe5920361e3593bc96f2b515e21f4c8f3d42
Ivan <ivan@spasov.xyz>
2023-02-15 21:03:15 +0200

    add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Logo from '../logo/Logo';
+import { Search } from '@mui/icons-material';
+import { Grid, InputBase, Button, Container, InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <Container className={styles.container}>
+        <Grid container alignItems="center">
+          <Grid item xs={2}>
+            <Logo />
+          </Grid>
+          <Grid item xs={6}>
+            <div>
+              {/* <Search className={styles.search}/>
+              <InputBase
+                className={styles.input}
+                placeholder="Find items, users and activities"
+              /> */}
+                <TextField
+                    className={styles.input}
+                    placeholder="Find items, users and activities"
+                    InputProps={{
+                        startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon style={{ color: 'white' }}/>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>
+              <Button color="inherit" className={styles.btn}>Home</Button>
+              <Button color="inherit" className={styles.btn}>Activity</Button>
+              <Button color="inherit" className={styles.btn}>Explore</Button>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
