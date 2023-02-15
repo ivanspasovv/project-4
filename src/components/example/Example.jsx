ec9d6cfaefd39ca14046f43c90e4790ef98ff043
Ivan <ivan@spasov.xyz>
2023-02-15 20:28:22 +0200

    add header

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -4,20 +4,11 @@ import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 import Logo from "../logo/Logo";
+import Header from "../header/Header";
 
 export default function Copyright() {
   return (
-    <div className={classNames(styles.wrapper)}>
-      <Logo />
-      <Logo type={"default"}/>
-      <Logo type={"muted"}/>
-      <Container className={classNames(styles.container)} maxWidth="xl">
-        <Paper className={classNames(styles.paper)}>
-          <Link href="/about" color="secondary">
-            Hi
-          </Link>
-        </Paper>
-      </Container>
-    </div>
+    <Header />
+    
   );
 }
