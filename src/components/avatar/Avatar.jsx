46dcf4227961cc0c615686e00109be8213ea2e8e
Ivan <ivan@spasov.xyz>
2023-02-16 16:47:49 +0200

    .

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 import styles from './Avatar.module.scss';
 
 export default function Avatar ({ size = 90, url, verified = false }) {
-  const badge = verified ? (
-    <div className={styles.badge}>
-      <img src="/images/verified.svg" alt="Verified" />
-    </div>
-  ) : null;
-
-  return (
-    <div className={styles.avatar} style={{ width: size, height: size }}>
-      <img className={styles.image} src={url} alt="Avatar" />
-      {badge}
-    </div>
-  );
-};
+    const badge = verified ? (
+      <div className={styles.badge}>
+        <div className={styles.badgeInner}>
+          <img src="/images/verified.svg" alt="Verified" />
+        </div>
+      </div>
+    ) : null;
+  
+    return (
+      <div className={styles.avatar} style={{ width: size, height: size }}>
+        <img className={styles.image} src={url} alt="Avatar" />
+        {badge}
+      </div>
+    );
+  };
 
 Avatar.propTypes = {
   size: PropTypes.number,
