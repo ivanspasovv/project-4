e61fd63a0f86c6c35434c90e29468145855c66f9
Ivan <ivan@spasov.xyz>
2023-02-16 21:11:19 +0200

    fix

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,46 +2,38 @@ import React from 'react';
 import { Card, CardMedia, CardContent, Typography, Chip } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import millify from 'millify';
-import User from "../user/User";
+import Avatar from "../avatar/Avatar";
+import styles from "./Card.module.scss";
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 345,
+    maxWidth: "18rem",
     margin: '5rem',
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', 
-  },
-  title: {
-    marginTop: '1rem',
-  },
-  price: {
-    fontWeight: 'bold',
-    marginTop: '1rem',
-  },
-  likes: {
-    marginTop: '1rem',
+    paddingTop: '100%', 
   },
 });
 
-const CardComponent = ({ name, likes, mediaUrl, user, price, currency }) => {
+export default function CardComponent ({ name, likes, mediaUrl, user, price, currency }) {
   const classes = useStyles();
     name = "Name";
     likes = 1500;
     mediaUrl = "/images/nft.jpg";
-//     user = {
-//     "avatar":{
-//        "url":"String"
-//     },
-//     "verified":"Boolean"
-//  };
     price = "~12.2";
     currency = "ETH";
+    user = {
+    avatar:{
+        url:"/images/avatar.png"
+    },
+        verified: true
+    };
+    
 
   return (
     <Card className={classes.card}>
-        <User />
+        <Avatar url={user.avatar.url} verified className={classes.avatar}/>
       <CardMedia
         className={classes.media}
         image={mediaUrl}
@@ -60,4 +52,3 @@ const CardComponent = ({ name, likes, mediaUrl, user, price, currency }) => {
   );
 };
 
-export default CardComponent;
