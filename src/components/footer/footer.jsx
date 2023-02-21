131262182633747b290198b03c9b047a2fc7f9c5
Ivan <ivan@spasov.xyz>
2023-02-21 22:04:05 +0200

    fix footer design

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,36 +1,32 @@
 import React from "react";
-import { Grid, Container, Typography, Button } from '@mui/material';
+import { Grid, Container, Button } from '@mui/material';
 import Logo from '../logo/Logo';
 import styles from "./Footer.module.scss";
 
-export default function Footer () {
-  return (
-      <div>
-          <Container maxWidth="lg" sx={{ pt: 6, pb: 2 }}>
-              <Grid container className={styles.container}>
-                  <Grid item xs={12} sm={6}>
-                      <Logo muted />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                      <Typography variant="body2" color="textSecondary" align="center">
-                          Some text describing the purpose of the footer
-                      </Typography>
-                      <Grid container justifyContent="center" spacing={2}>
-                          <Grid item>
-                              <Button color="inherit" href="/privacy-policy">
-                                  Privacy Policy
-                              </Button>
-                          </Grid>
-                          <Grid item>
-                              <Button color="inherit" href="/cookie-policy">
-                                  Cookie Policy
-                              </Button>
-                          </Grid>
-                      </Grid>
-                  </Grid>
-              </Grid>
-          </Container>
-      </div>
-      
+export default function Footer() {
+    return (
+        <div>
+            <Container>
+                <Grid container className={styles.container}>
+                    <Grid>
+                        <Logo type="muted" />
+                    </Grid>
+                    <Grid className={styles.text}>
+                        <span>
+                            Some text describing the purpose of the footer
+                        </span>
+                    </Grid>
+                    <Grid className={styles.buttons}>
+                        <Button color="inherit" href="/privacy-policy" className={styles.buttons}>
+                            Privacy Policy
+                        </Button>
+                        <Button color="inherit" href="/cookie-policy">
+                            Cookie Policy
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+
     );
 };
