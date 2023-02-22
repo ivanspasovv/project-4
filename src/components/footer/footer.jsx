c00fb187e3d555a06cce34ad9985c64bc8125ecd
Ivan <ivan@spasov.xyz>
2023-02-22 21:00:40 +0200

    .

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Grid, Container, Button } from '@mui/material';
 import Logo from '../logo/Logo';
 import styles from "./Footer.module.scss";
@@ -7,15 +8,15 @@ export default function Footer() {
         <div>
             <Container>
                 <Grid container className={styles.container}>
-                    <Grid>
+                    <Grid item >
                         <Logo type="muted" />
                     </Grid>
-                    <Grid className={styles.text}>
+                    <Grid item className={styles.text}>
                         <span>
                             Some text describing the purpose of the footer
                         </span>
                     </Grid>
-                    <Grid>
+                    <Grid item >
                         <Button color="inherit" href="/privacy-policy" className={styles.buttons}>
                             Privacy Policy
                         </Button>
