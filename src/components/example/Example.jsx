948895404541fc0347344befcabc9a63945e95cb
Ivan <ivan@spasov.xyz>
2023-02-27 17:16:20 +0200

    .

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,6 +1,6 @@
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
-import Footer from "../footer/footer";
+import Footer from "../footer/Footer";
 
 export default function Copyright() {
   return (
