102b26a0142cdaae2808d8c31087780732da489e
Ivan <ivan@spasov.xyz>
2023-02-27 20:42:38 +0200

    .

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,11 +1,13 @@
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import Step from "../how/Step";
+import How from "../how/How";
 
 export default function Copyright() {
   return (
     <div>
       <Step />
+      <How />
     </div>
   );
 }
