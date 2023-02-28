6e79031a219f3e7d45ebbe4c4349984f25b104b4
Ivan <ivan@spasov.xyz>
2023-02-28 21:01:55 +0200

    add prop

--- src/components/collectors/Collector.jsx
+++ src/components/collectors/Collector.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import User from "../user/User";
 import styles from "./Collector.module.scss";
 
-export default function Collector ({ name = "", avatar = "", verified = false, type }) {
+export default function Collector ({ name = "", avatar = "", verified = false, type, nftsCount = 0 }) {
   return (
     <div className={`${styles.container} ${type === "light" ? styles.light : styles.dark}`}>
       <User name={name} avatar={avatar} verified={verified} />
