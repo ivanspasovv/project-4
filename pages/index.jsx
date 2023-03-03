5a1298f27ccc51c77f82ba95093ab2e854152074
Ivan <ivan@spasov.xyz>
2023-03-03 20:07:01 +0200

    add components in index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,21 @@
-import Example from "../src/components/example/Example";
+import Header from "../src/components/header/Header";
+import Featured from "../src/components/featured/Featured";
+import Trending from "../src/components/trending/Trending";
+import TopCollectors from "../src/components/collectors/TopCollectors";
+import How from "../src/components/how/How";
+import Auctions from "../src/components/auctions/Auctions";
+import Footer from "../src/components/footer/Footer";
 
 export default function Index() {
-  return <Example />;
-}
+  return (
+    <div>
+    <Header />
+    <Featured />
+    <Trending />
+    <TopCollectors />
+    <How />
+    <Auctions />
+    <Footer />
+  </div>
+  );
+};
