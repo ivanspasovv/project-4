f387d24faaf6cefe9fd957e5cf2887064a46b605
Ivan <ivan@spasov.xyz>
2023-03-08 21:30:33 +0200

    .

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Typography} from '@mui/material';
+import Typography from '@mui/material/Typography';
 import styles from './ProductInfoTitle.module.scss';
 
 export default function ProductInfoTitle ({ text, title = "My NFT Name"}) {
   text = title
   return (
   <div className={styles['product-info-title']}>
-    <Typography className={styles.title}>
-      {text}{title}
+    <Typography variant="h5" className={styles.title}>
+      {text}
+    </Typography>
+    <Typography>
+      {title}
     </Typography>
   </div>
   );
