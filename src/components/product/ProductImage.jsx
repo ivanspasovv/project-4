6fe807b34d89f930ecf7f507453c34bfc6ed1760
Ivan <ivan@spasov.xyz>
2023-03-08 19:38:48 +0200

    add product image component

---
+++ src/components/product/ProductImage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ProductImage.module.scss';
+
+export default function ProductImage ({ URL }) {
+  return (
+    <div className={styles.product-image}>
+      <img className={styles.image} src={URL} alt="Product" />
+    </div>
+  );
+};
