cac2a363684bbd9e6783184f74c52d818ac09e15
Ivan <ivan@spasov.xyz>
2023-03-08 21:04:54 +0200

    .

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -6,7 +6,7 @@ export default function ProductInfoTitle ({ text, title = "My NFT Name"}) {
   text = title
   return (
   <div className={styles['product-info-title']}>
-    <Typography variant="h5" component="h2" className={styles.title} >
+    <Typography variant="h5" className={styles.title} >
       {text}
     </Typography>
   </div>
