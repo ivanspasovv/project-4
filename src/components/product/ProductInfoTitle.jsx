5ca8a5f6d37e2feccd2ce47472d4d97e29b2c50c
Ivan <ivan@spasov.xyz>
2023-03-08 20:28:22 +0200

    .

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+import {Typography} from '@mui/material';
 import styles from './ProductInfoTitle.module.scss';
 
 export default function ProductInfoTitle ({ text }) {
